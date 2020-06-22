7b8ad3065ae40394a6e42290b8edd7574dc398ba
Jesse Clark <hello@developerjesse.com>
2020-06-22 12:17:32 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a5d9f762ca05ea8eb03ff0ece72f5272.js"
+  "/precache-manifest.0fd0394239c2bad5782acc26b761cc34.js"
 );
 
 workbox.clientsClaim();
